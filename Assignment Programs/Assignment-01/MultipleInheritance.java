8566058bd910c7391a004b0aee53dc42ee72edde
kandelliseshubabu25csm-coder <kandelliseshubabu.25.csm@anits.edu.in>
2026-08-26 22:42:09 +0530

    Add files via upload

---
+++ Assignment Programs/Assignment-01/MultipleInheritance.java
@@ -0,0 +1,29 @@
+package javaproject;
+interface Father {
+    void house();
+}
+
+interface Mother {
+    void car();
+}
+
+class Target implements Father, Mother {
+
+    public void house() {
+        System.out.println("Father has a house");
+    }
+
+    public void car() {
+        System.out.println("Mother has a car");
+    }
+}
+public class MultipleInheritance {
+
+    public static void main(String[] args) {
+
+        Target t = new Target();
+
+        t.house();
+        t.car();
+    }
+}
