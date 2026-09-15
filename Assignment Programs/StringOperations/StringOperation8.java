52e39d796b2a10f949b6453cbe9a418b0acacb00
kandelliseshubabu25csm-coder <kandelliseshubabu.25.csm@anits.edu.in>
2026-09-15 18:35:55 +0530

    Add files via upload

---
+++ Assignment Programs/StringOperations/StringOperation8.java
@@ -0,0 +1,12 @@
+package packagecode.java;
+
+public class StringOperation8 {
+	public static void main(String[] args) {
+	        String n = "   seshu ";
+	        String trim = n.trim();
+	        System.out.println(n); 
+	        System.out.println(trim);
+	    }
+	}
+
+
