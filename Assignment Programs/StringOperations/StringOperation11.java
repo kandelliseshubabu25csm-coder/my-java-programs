5bc1db5f31392412d7a62e94cdaba3b73545928a
kandelliseshubabu25csm-coder <kandelliseshubabu.25.csm@anits.edu.in>
2026-09-15 18:37:58 +0530

    Add files via upload

---
+++ Assignment Programs/StringOperations/StringOperation11.java
@@ -0,0 +1,14 @@
+package packagecode.java;
+
+public class StringOperation11 {
+	 public static void main(String[] args) {
+		    String m = "SESHU";
+		        String t = "seshu";
+		        boolean result = m.equalsIgnoreCase(t);
+		        System.out.println("string one is " + m);
+		        System.out.println("second string is " + t);
+		        System.out.println("the ignoring case " + result);
+		    }
+		}
+
+
