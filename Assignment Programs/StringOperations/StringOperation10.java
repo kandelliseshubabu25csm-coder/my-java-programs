15a5140e8ebbeb26bb9c62e837c818188bbf359f
kandelliseshubabu25csm-coder <kandelliseshubabu.25.csm@anits.edu.in>
2026-09-15 18:37:32 +0530

    Add files via upload

---
+++ Assignment Programs/StringOperations/StringOperation10.java
@@ -0,0 +1,13 @@
+package packagecode.java;
+
+public class StringOperation10 {
+	 public static void main(String[] args) {
+	        String t = "SESHU";
+	        String i = "ravi";
+	        String e = "Ramana";
+	    System.out.println("t is euals to i is "  + t.equals(i));  
+	    System.out.println("t is euals to  e is " + t.equals(e));
+	  }
+	}
+
+
