52cdef26222d8eb072d5e06261eeadd22bf05171
kandelliseshubabu25csm-coder <kandelliseshubabu.25.csm@anits.edu.in>
2026-09-15 18:29:47 +0530

    Add files via upload

---
+++ Assignment Programs/StringOperations/StringOperation1.java
@@ -0,0 +1,12 @@
+package packagecode.java;
+
+public class StringOperation1 {
+	
+	    public static void main(String[] args) {
+	        String a = "seshu218";
+	        int length = a.length();
+	        System.out.println("String is" + a);
+	        System.out.println("string length is" + length);
+	    }
+	}
+
