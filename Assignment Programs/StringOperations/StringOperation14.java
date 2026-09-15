f032585a83f254ff1c7329fdbbe3acc456d26e33
kandelliseshubabu25csm-coder <kandelliseshubabu.25.csm@anits.edu.in>
2026-09-15 18:39:32 +0530

    Add files via upload

---
+++ Assignment Programs/StringOperations/StringOperation14.java
@@ -0,0 +1,22 @@
+package packagecode.java;
+
+public class StringOperation14 {
+    public static void main(String[] args) {
+        // concatentation is done the operation
+       String  o = "seshu";
+       String p = " babu";
+       String result = o.concat(p);
+       System.out.println("the string is given " + o );
+       System.out.println("second string is "  + p);
+       System.out.println("the resutlt of concatenation is "   + result);
+
+
+   
+
+
+
+
+
+    }
+
+}
