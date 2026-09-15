95b949f6d6e4c3ba3f81b1e23b7a3ce3246634cc
kandelliseshubabu25csm-coder <kandelliseshubabu.25.csm@anits.edu.in>
2026-09-15 17:52:20 +0530

    Add files via upload

---
+++ Assesment Programs/Internal-Assesment-01/StringOperation2.java
@@ -0,0 +1,18 @@
+package packagecode.java;
+
+public class StringOperation2 {
+ // char  at program is second program
+	    public static void main(String[] args) {
+	        String mine = "seshu218";
+	        char s = mine.charAt(3);
+	        int len = mine.length();
+	        char c = mine.charAt(8);
+
+	        System.out.println("what the character is " + s);
+	        System.out.println("the given string length" + len);
+	        System.out.println("the characterof string is" + c);
+	    }
+	}
+	
+
+
