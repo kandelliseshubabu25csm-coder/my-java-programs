40462da8eeb205daf56e0c40d3926a3b3a4af096
kandelliseshubabu25csm-coder <kandelliseshubabu.25.csm@anits.edu.in>
2026-09-15 17:54:22 +0530

    Add files via upload

---
+++ Assesment Programs/Internal-Assesment-01/StringOperation7.java
@@ -0,0 +1,13 @@
+package packagecode.java;
+
+public class StringOperation7 {
+	
+	    public static void main(String[] args) {
+	        String o = "hello windows";
+	        String re = o.replace("e","a" );
+	        System.out.println(o);
+	        System.out.println(re);
+	    }
+	}
+
+
