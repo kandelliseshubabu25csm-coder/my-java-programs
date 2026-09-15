cdd9f524b0b68cc02df17d3a1112264cfc2a0cf7
kandelliseshubabu25csm-coder <kandelliseshubabu.25.csm@anits.edu.in>
2026-09-15 18:34:32 +0530

    Add files via upload

---
+++ Assignment Programs/StringOperations/StringOperation6.java
@@ -0,0 +1,24 @@
+package packagecode.java;
+
+public class StringOperation6 {
+	
+	    public static void main(String[] args) {
+	        
+	        String m = "Hello Seshu Babu";
+	        String up = m.toUpperCase();
+	        String  lc = m.toLowerCase();
+	        System.out.println(m);
+	        System.out.println(up);
+	        System.out.println(lc);
+
+
+
+
+
+
+
+
+	    }
+	}
+
+
