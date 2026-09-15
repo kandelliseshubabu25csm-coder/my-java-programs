a824ea4f603e8e86c1e6e5c2e8b75486a4291e9d
kandelliseshubabu25csm-coder <kandelliseshubabu.25.csm@anits.edu.in>
2026-09-15 18:37:03 +0530

    Add files via upload

---
+++ Assignment Programs/StringOperations/StringOperation9.java
@@ -0,0 +1,11 @@
+package packagecode.java;
+
+
+public class StringOperation9 {
+    public static void main(String[] args) {
+        String z = " seshubabu 218 ";
+        String [] gap = z.split(",");
+        System.out.println("originalstring is " + z);
+        System.out.println("spilt the string" + z);
+    }
+}
