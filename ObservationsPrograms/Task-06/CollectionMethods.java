8cf59709b34c6e9bab7363bf0c44ee82b2ff4db5
kandelliseshubabu25csm-coder <kandelliseshubabu.25.csm@anits.edu.in>
2026-09-23 18:52:09 +0530

    Add files via upload

--- ObservationsPrograms/Task-06/CollectionMethods.java
+++ ObservationsPrograms/Task-06/CollectionMethods.java
@@ -1,5 +1,5 @@
-
-    import java.util.*;
+package packagecode.java;
+import java.util.*;
 
 public class CollectionMethods {
 
@@ -64,5 +64,3 @@ public class CollectionMethods {
     }
 }
 	
-    
-
